c48dcd5da6e7eae5dcbf86ba12d2d0fb80d5ef26
Alamgir Kabir Roni <kabir@mahdil.com>
2019-01-27 23:25:24 +0600

    home page slider added

--- src/page/static/page/js/home.js
+++ src/page/static/page/js/home.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-
+    var swiper = new Swiper('.blog-slider', {
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true,
+        mousewheel: {
+            invert: false,
+        },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        // autoHeight: true,
+        pagination: {
+            el: '.blog-slider__pagination',
+            clickable: true,
+        }
+    });
 });
